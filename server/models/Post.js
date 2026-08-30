9d4b1b3d409d22e3fd0946154fba8739b5e7230b
vivekraut78 <vivekraut7707@gmail.com>
2026-08-30 22:03:55 +0530

    Add Mongoose models for User, Post, and Comment

---
+++ server/models/Post.js
@@ -0,0 +1,19 @@
+mongoose.Schema
+(
+ {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: 
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    tags: [String],
+    likes: { type: Number, default: 0 },
+  },
+  
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Post", postSchema);
